3e79259dcecf01e279799174fb6d7861322a5569
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-02-14 01:36:41 +0300

    [feat] implement logo to home URL

---
+++ dapp/src/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+
+import { ReactComponent as BitcoinIcon } from 'assets/icons/bitcoin.svg';
+
+const Logo = (props: any) => (
+  <BitcoinIcon {...props} />
+);
+
+export default Logo;
--- dapp/src/parts/AppBar/index.tsx
+++ dapp/src/parts/AppBar/index.tsx
@@ -1,15 +1,20 @@
 
-import { ReactComponent as BitcoinIcon } from 'assets/icons/bitcoin.svg';
+import { Link } from 'react-router-dom';
+
+import Logo from 'components/Logo';
+import { PAGES } from 'utils/constants/links';
 import styles from './app-bar.module.css';
 
 const AppBar = () => (
   <header className={styles['app-bar']}>
-    <BitcoinIcon
-      style={{
-        marginRight: 8
-      }}
-      width={60}
-      height={60} />
+    <Link to={PAGES.HOME}>
+      <Logo
+        style={{
+          marginRight: 8
+        }}
+        width={60}
+        height={60} />
+    </Link>
     <h1>
       Bitcoin Fork Explorer
     </h1>
