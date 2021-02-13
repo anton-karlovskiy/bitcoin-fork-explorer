5a803a18e0ae055aa2b581569812354ea426c8fb
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-02-13 23:46:55 +0300

    * [feat] add a logo icon to the app bar * [fix] correct react-app-env.d.ts

--- dapp/src/parts/AppBar/index.tsx
+++ dapp/src/parts/AppBar/index.tsx
@@ -1,6 +1,15 @@
 
+import { ReactComponent as BitcoinIcon } from 'assets/icons/bitcoin.svg';
+import styles from './app-bar.module.css';
+
 const AppBar = () => (
-  <header>
+  <header className={styles['app-bar']}>
+    <BitcoinIcon
+      style={{
+        marginRight: 8
+      }}
+      width={60}
+      height={60} />
     <h1>
       Bitcoin Fork Explorer
     </h1>
