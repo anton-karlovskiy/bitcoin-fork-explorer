50d442f9d77e0cad26a1627fabfda312337b2194
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-02-15 21:24:38 +0300

    [feat] update the styling of error report component

--- dapp/src/components/ErrorReport/index.tsx
+++ dapp/src/components/ErrorReport/index.tsx
@@ -1,11 +1,13 @@
 
+import styles from './error-report.module.css';
+
 interface Props {
   error: Error | undefined
 }
 
 // TODO: could be polished
 const ErrorReport = ({ error }: Props) => (
-  <div>{error?.message}</div>
+  <div className={styles['error-report']}>{error?.message}</div>
 );
 
 export default ErrorReport;
